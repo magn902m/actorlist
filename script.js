928078ff1e82364ff6b1958bcc7a08c9fc22c367
Magnus <magn902m@stud.kea.dk>
2022-02-01 22:10:13 +0100

    added popup on img, and styled header etc

--- script.js
+++ script.js
@@ -22,7 +22,22 @@ function viewActorsList() {
     let clon = temp.cloneNode(true).content;
     // document.querySelector(".image").innerHTML += `<img src="${movie.svg}" alt="movie icon">`;
     clon.querySelector(".fullname").textContent = actor.fullname;
-    clon.querySelector(".movie").textContent = actor.movie;
+    clon.querySelector(".image").addEventListener("click", () => showDetails(actor));
     container.appendChild(clon);
   });
+}
+
+document.querySelector("#popup button").addEventListener("click", closePopup);
+
+function closePopup() {
+  document.querySelector("#popup").style.display = "none";
+}
+
+function showDetails(actorDetails) {
+  console.log("popup open");
+
+  const popup = document.querySelector("#popup");
+  popup.style.display = "block";
+  popup.querySelector(".fullname").textContent = actorDetails.fullname;
+  popup.querySelector(".movie").textContent = actorDetails.movie;
 }
